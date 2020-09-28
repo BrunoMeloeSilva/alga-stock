70c5a6a954c3e4020db55d53b57212af70d66d8f
brunomeloesilva <brunomeloesilva@gmail.com>
2020-09-27 20:59:01 -0400

    Criando componente Header e resetando css

---
+++ src/components/App/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../Header/Header';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header title="AlgaStock"/>
+    </div>
+  );
+}
+
+export default App;
