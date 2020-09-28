41300be8b22e216c4154b739b5f6ef91a6fa9a9e
brunomeloesilva <brunomeloesilva@gmail.com>
2020-09-28 06:46:37 -0400

    Criando um subtipo de input para tipar minha props

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,6 +3,7 @@ import Button from '../shared/Button/Button';
 import Header from '../Header/Header';
 import './App.css';
 import Container from '../shared/Container/Container';
+import Input from '../shared/Input/Input';
 
 function TestComponent() {
   return <div>oi</div>
@@ -17,6 +18,10 @@ function App() {
           appendIcon={ <TestComponent/> }>
           props.children vem do React.FC 
         </Button>
+        <Input 
+          label="Street"
+          placeholder="Rua x, 123 - Bairro"
+        />
       </Container>
     </div>
   );
