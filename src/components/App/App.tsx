3354ce5e3de929369c53c6a24dd6b54b41e053f7
brunomeloesilva <brunomeloesilva@gmail.com>
2020-10-04 16:02:43 -0400

    3.6. Atualizando o schema do produto

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -31,7 +31,7 @@ function App() {
     setProducts([
       ...products,
       {
-        id: products.length + 1,
+        _id: String(products.length + 1),
         ...product
       }
     ])
@@ -39,7 +39,7 @@ function App() {
 
   const handleProductUpdate = (newProduct: Product) => {
     setProducts(products.map(product =>
-      product.id === newProduct.id
+      product._id === newProduct._id
         ? newProduct
         : product
     ))
@@ -47,8 +47,8 @@ function App() {
     setUpdatingProduct(undefined)
   }
 
-  const deleteProduct = (id: number) => {
-    setProducts(products.filter(product => product.id !== id))
+  const deleteProduct = (id: string) => {
+    setProducts(products.filter(product => product._id !== id))
   }
 
   const handleProductDelete = (product: Product) => {
@@ -64,7 +64,7 @@ function App() {
       })
       .then((result) => {
         if (result.value) {
-          deleteProduct(product.id)
+          deleteProduct(product._id)
           Swal.fire(
             'Deleted!',
             'Your file has been deleted.',
