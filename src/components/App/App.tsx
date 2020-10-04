898114ea9526bbdb3f8d5d2e2e62733444ec4a65
brunomeloesilva <brunomeloesilva@gmail.com>
2020-10-04 15:47:21 -0400

    3.5. Entendendo o pattern de serviços

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,12 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Swal from 'sweetalert2'
 import './App.css';
 import Header from '../Header';
 import Container from '../../shared/Container';
 import Table, { TableHeader } from '../../shared/Table';
-import Products, { Product } from '../../shared/Table/Table.mockdata';
+import { Product } from '../../shared/Table/Table.mockdata';
 import ProductForm, { ProductCreator } from '../Products/ProductForm';
+import { getAllProducts } from '../../services/Products.service';
 
 const headers: TableHeader[] = [
   { key: 'id', value: '#' },
@@ -15,8 +16,16 @@ const headers: TableHeader[] = [
 ]
 
 function App() {
-  const [products, setProducts] = useState(Products)
+  const [products, setProducts] = useState<Product[]>([])
   const [updatingProduct, setUpdatingProduct] = useState<Product | undefined>(products[0])
+
+  useEffect(() => {
+    async function fetchData() {
+      const _products = await getAllProducts() //Nesse ponto a aplicacao PARA e fica aguardando a resposta !
+      setProducts(_products)
+    }
+    fetchData()
+  },[])
   
   const handleProductSubmit = (product: ProductCreator) => {
     setProducts([
